9c9f10f3555799c756af83fd4c2cb1cb8f9a313e
mhmdfarhan28 <mhmdfarhan28@gmail.com>
2025-05-04 14:14:55 +0700

    create book model, book controller, database factories, index and layout views 60%

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,13 +19,32 @@ class Book extends Model
         return $query->where('title', 'LIKE', '%'.$title.'%');
     }
 
-    public function scopePopular(Builder $query): Builder {
-        return $query->withCount('reviews')
+    // the counts of recent review between the specific dates
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder {
+        return $query->withCount([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+            ])
             ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query): Builder {
-        return $query->withAvg('reviews', 'rating')
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder {
+        return $query->withAvg([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+            ], 'rating')
             ->orderBy('reviews_avg_rating','desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder {
+        return $query->having('reviews_count','>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null) {
+        if($from && !$to) {
+            $query->where('created_at','>=', $from);
+        } else if (!$from && $to) {
+            $query->where('created_at','<=', $to);
+        } else if ($from && $to) {
+            $query->whereBetween('created_at',[$from, $to]);
+        }
     }
 }
--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -16,11 +16,12 @@ class BookFactory extends Factory
      */
     public function definition(): array
     {
+        $createdAt = fake()->dateTimeBetween('-2 years');
         return [
             'title' => fake()->sentence(3),
             'author' => fake()->name(),
-            'created_at' => fake()->dateTimeBetween('-2 years'),
-            'updated_at' => fake()->dateTimeBetween('created_at', 'now')
+            'created_at' => $createdAt,
+            'updated_at' => fake()->dateTimeBetween($createdAt, 'now')
         ];
     }
 }
